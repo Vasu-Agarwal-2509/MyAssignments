307055cb0865e757e544bfe84b0732f99e4ca436
Vasu <vasu.agarwal_cs19@gla.ac.in>
2023-05-16 18:32:59 +0530

    Question2 of Generics assignment

---
+++ GenericsAssignment/Assignment4Q2.java
@@ -0,0 +1,19 @@
+package GenericsAssignment;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class Assignment4Q2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        HashMap<Integer,Double> hm = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            int key = random.nextInt(20) + 1;
+            double value = random.nextDouble(20) + 1;
+            hm.put(key, value);
+        }
+        for (int k: hm.keySet()) {
+            System.out.println(k + " " + hm.get(k));
+        }
+    }
+}
