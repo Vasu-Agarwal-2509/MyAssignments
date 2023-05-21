195ee5ca03789704a1bc477c49632b09ab82f164
Vasu <vasu.agarwal_cs19@gla.ac.in>
2023-05-21 22:55:02 +0530

    Question3 of Generics assignment

---
+++ GenericsAssignment/Assignment4Q3.java
@@ -0,0 +1,15 @@
+package GenericsAssignment;
+
+public class Assignment4Q3 {
+    public static void main(String[] args) {
+        Pair pairObj = new Pair();
+        pairObj.setKey("1");
+        pairObj.setValue("Hello");
+
+        Pair pairObj2 = new Pair();
+        pairObj2.setKey("Today is");
+        pairObj2.setValue(String.valueOf(new java.util.Date()));
+
+        System.out.println(pairObj2.getValue());
+    }
+}
