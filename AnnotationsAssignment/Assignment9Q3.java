fc0a23e90a75d723f4b6050afec24c8c8857f557
Vasu <vasu.agarwal_cs19@gla.ac.in>
2023-05-22 17:14:01 +0530

    Question3 of Annotation assignment

---
+++ AnnotationsAssignment/Assignment9Q3.java
@@ -0,0 +1,13 @@
+package AnnotationsAssignment;
+
+public class Assignment9Q3 {
+
+    @Execute(sequence = 2)
+    public void myMethod1(){}
+
+    @Execute(sequence = 1)
+    public void myMethod2(){}
+
+    @Execute(sequence = 3)
+    public void myMethod3(){}
+}
