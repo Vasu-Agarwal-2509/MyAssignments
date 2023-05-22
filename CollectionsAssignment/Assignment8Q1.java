55c124a5fa04cc2a08f6c2c050e888925120d255
Vasu <vasu.agarwal_cs19@gla.ac.in>
2023-05-22 18:14:33 +0530

    Question1 of Collection assignment

--- CollectionsAssignment/Assignment8Q1.java
+++ CollectionsAssignment/Assignment8Q1.java
@@ -3,26 +3,27 @@ import java.util.*;
 
 public class Assignment8Q1 {
     public static void main(String[] args) {
-        Contact ct1 = new Contact(908443953, "Vasu", "vasu@gmail.com", gender.MALE);
-        Contact ct2 = new Contact(353762584, "Rahul", "rahul@gmail.com", gender.MALE);
-        Contact ct3 = new Contact(678323222, "Shaurya", "shaurya@gmail.com", gender.MALE);
+        Contact contactObj1 = new Contact(908443953, "Vasu", "vasu@gmail.com", gender.MALE);
+        Contact contactObj2 = new Contact(353762584, "Rahul", "rahul@gmail.com", gender.MALE);
+        Contact contactObj3 = new Contact(678323222, "Shaurya", "shaurya@gmail.com", gender.MALE);
 
-        Map<Long, Contact> mp = new TreeMap<Long, Contact>(new SortByPhoneNumber());
-        mp.put(ct1.phoneNumber, ct1);
-        mp.put(ct2.phoneNumber, ct2);
-        mp.put(ct3.phoneNumber, ct3);
+        Map<Long, Contact> treeMap = new TreeMap<Long, Contact>(Collections.reverseOrder()); //new SortByPhoneNumber()
+        treeMap.put(contactObj1.phoneNumber, contactObj1);
+        treeMap.put(contactObj2.phoneNumber, contactObj2);
+        treeMap.put(contactObj3.phoneNumber, contactObj3);
 
-        for(long phoneNum : mp.keySet())
+
+        for(long phoneNum : treeMap.keySet())
             System.out.println("Keys: " + phoneNum);
         System.out.println();
-        for (long phoneNum : mp.keySet()) {
-            Contact contactObj = mp.get(phoneNum);
-            System.out.println("Values: " + contactObj.getContactDetails(contactObj));
+        for (long phoneNum : treeMap.keySet()) {
+            Contact contactObj = treeMap.get(phoneNum);
+            System.out.println("Values: " + contactObj);
         }
         System.out.println();
-        for (long phoneNum : mp.keySet()) {
-            Contact contactObj = mp.get(phoneNum);
-            System.out.println("Keys: " + phoneNum + " Values: " + contactObj.getContactDetails(contactObj));
+        for (long phoneNum : treeMap.keySet()) {
+            Contact contactObj = treeMap.get(phoneNum);
+            System.out.println("Keys: " + phoneNum + " Values: " + contactObj);
         }
     }
 }
